0ddc9c80d5b1f8a3b7c36d8513735c4c6272785d
Matthew LaRocca <mlarocca.cpp@gmail.com>
2019-05-13 20:48:13 -0700

    Move event processing logic into common class

--- src/event.h
+++ src/event.h
@@ -4,13 +4,17 @@
 
 #include <queue>
 #include <mutex>
+#include <cstdint>
 
 namespace Ezmidi {
 	
 class EventQueue{
 public:
-	void pushNote(int status, int key, int velocity);
+    void processMessage(const uint8_t* data);
 	int pumpEvents(Ezmidi_Event& event);
+    
+private:
+    void processNoteEvent(const uint8_t* data);
 	
 private:
 	std::queue<Ezmidi_Event> event_queue;
