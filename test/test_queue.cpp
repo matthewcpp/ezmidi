24c2233b149ffc7c3df9d157dc867bdd9f6c41d3
Matthew LaRocca <mlarocca.cpp@gmail.com>
2019-05-18 16:12:06 -0700

    Add initial tests

---
+++ test/test_queue.cpp
@@ -0,0 +1,46 @@
+#include "gtest/gtest.h"
+#include "event.h"
+#include "ezmidi.h"
+#include "midi.h"
+
+#include <array>
+
+using MidiNoteMessage = std::array<uint8_t, 3>;
+
+TEST(Queue, PumpEmpty)
+{
+	Ezmidi::EventQueue event_queue;
+
+	Ezmidi_Event event;
+	ASSERT_EQ(event_queue.pumpEvents(event), 0);
+}
+
+TEST(Queue, Pump)
+{
+	MidiNoteMessage message = { Midi::Status::NoteOnChannel1, 60, 11 };
+
+	Ezmidi::EventQueue event_queue;
+	event_queue.processMessage(message.data());
+
+	Ezmidi_Event event;
+	ASSERT_NE(event_queue.pumpEvents(event), 0);
+	ASSERT_EQ(event_queue.pumpEvents(event), 0);
+}
+
+TEST(Queue, FIFO)
+{
+	Ezmidi::EventQueue event_queue;
+
+	MidiNoteMessage message1 = { Midi::Status::NoteOnChannel1, 60, 11 };
+	MidiNoteMessage message2 = { Midi::Status::NoteOnChannel1, 61, 11 };
+
+	event_queue.processMessage(message1.data());
+	event_queue.processMessage(message2.data());
+
+	Ezmidi_Event event;
+	ASSERT_NE(event_queue.pumpEvents(event), 0);
+	ASSERT_EQ(event.note_event.note, message1[1]);
+
+	ASSERT_NE(event_queue.pumpEvents(event), 0);
+	ASSERT_EQ(event.note_event.note, message2[1]);
+}
